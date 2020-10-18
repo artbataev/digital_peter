72b024b29e802d73c2e29d2a72de88e93ce25cd7
artbataev <artbataev@gmail.com>
2020-10-18 22:35:34 +0300

    Fix model

--- digital_peter/models.py
+++ digital_peter/models.py
@@ -1,10 +1,11 @@
 import torch.nn as nn
+import torch.nn.utils.rnn as utils_rnn
 
 
 class BaselineModel(nn.Module):
     def __init__(self, num_outputs):
         super().__init__()
-        # input: Bx3x128,1024
+        # input: Bx3x128xL
         self.encoder = nn.Sequential(*[
             nn.Conv2d(3, 64, kernel_size=(3, 3), padding=1),
             nn.ReLU(),
@@ -30,15 +31,17 @@ class BaselineModel(nn.Module):
             nn.ReLU(),
         ])
 
-        self.rnn = nn.Sequential(*[
-            nn.Dropout(0.2),
-            nn.GRU(input_size=512, hidden_size=256, bidirectional=True, dropout=0.2, batch_first=True, num_layers=2)
-        ])
+        self.rnn_dropout = nn.Dropout(0.2)
+        self.rnn = nn.GRU(input_size=512, hidden_size=256, bidirectional=True, dropout=0.2, batch_first=False,
+                          num_layers=2)
         self.final = nn.Linear(512, num_outputs)
 
-    def forward(self, x):
-        output = self.encoder(x)  # Bx512x1x255
-        output = output.squeeze(dim=2).transpose(1, 2)  # BxLxC
+    def forward(self, images, image_lengths):
+        output = self.encoder(images)  # Bx512x1x(L/4-1)
+        output = output.squeeze(dim=2).permute(2, 0, 1)  # LxBxC
+        output = self.rnn_dropout(output)
+        output = utils_rnn.pack_padded_sequence(output, image_lengths // 4 - 1, batch_first=False)
         output = self.rnn(output)[0]
+        output = utils_rnn.pad_packed_sequence(output)[0]
         logits = self.final(output)
-        return logits
+        return logits  # LxBxC
