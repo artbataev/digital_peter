b7574a7d6aacfeec4122be7b3112a93a6b5beaa9
artbataev <artbataev@gmail.com>
2020-11-04 23:47:19 +0300

    Seq2seq: bugfixes

--- digital_peter/models/seq2seq.py
+++ digital_peter/models/seq2seq.py
@@ -46,7 +46,6 @@ class Attention(nn.Module):
             self.linear_in = nn.Linear(dimensions, dimensions, bias=False)
 
         self.linear_out = nn.Linear(dimensions * 2, dimensions, bias=False)
-        self.softmax = nn.Softmax(dim=-1)
         self.tanh = nn.Tanh()
 
     def forward(self, query, context, context_lengths=None):
@@ -64,29 +63,26 @@ class Attention(nn.Module):
             * **weights** (:class:`torch.FloatTensor` [batch size, output length, query length]):
               Tensor containing attention weights.
         """
-        batch_size, output_len, dimensions = query.size()
-        query_len = context.size(1)
+        batch_size, output_len, dimensions = query.shape
+        context_len = context.shape[1]
 
         if self.attention_type == "general":
             query = query.reshape(batch_size * output_len, dimensions)
             query = self.linear_in(query)
             query = query.reshape(batch_size, output_len, dimensions)
 
-        # TODO: Include mask on PADDING_INDEX?
-
         # (batch_size, output_len, dimensions) * (batch_size, query_len, dimensions) ->
-        # (batch_size, output_len, query_len)
+        # (batch_size, output_len, context_len)
         attention_scores = torch.bmm(query, context.transpose(1, 2).contiguous())
 
         if context_lengths is not None:
-            mask = torch.arange(query_len, dtype=torch.long, device=context_lengths.get_device()
-                                ).expand(batch_size, query_len) < context_lengths
-            attention_scores = attention_scores.masked_fill(mask, float("-inf"))
+            device = context_lengths.get_device()
+            mask = torch.arange(context_len, dtype=torch.long, device=device
+                                ).unsqueeze(0).expand(batch_size, -1) >= context_lengths.unsqueeze(1)
+            attention_scores = attention_scores.masked_fill(mask.unsqueeze(1).expand(-1, output_len, -1), float("-inf"))
 
         # Compute weights across every context sequence
-        attention_scores = attention_scores.view(batch_size * output_len, query_len)
-        attention_weights = self.softmax(attention_scores)
-        attention_weights = attention_weights.view(batch_size, output_len, query_len)
+        attention_weights = F.softmax(attention_scores, dim=-1)
 
         # (batch_size, output_len, query_len) * (batch_size, query_len, dimensions) ->
         # (batch_size, output_len, dimensions)
@@ -94,24 +90,25 @@ class Attention(nn.Module):
 
         # concat -> (batch_size * output_len, 2*dimensions)
         combined = torch.cat((mix, query), dim=2)
-        combined = combined.view(batch_size * output_len, 2 * dimensions)
 
         # Apply linear_out on every 2nd dimension of concat
         # output -> (batch_size, output_len, dimensions)
-        output = self.linear_out(combined).view(batch_size, output_len, dimensions)
+        output = self.linear_out(combined)
         output = self.tanh(output)
 
         return output, attention_weights
 
 
 class AttnDecoderRnn(nn.Module):
-    def __init__(self, output_size, hidden_size, rnn_layers=(0, 2), embedding_dropout_p=0.1, sos=0, eos=1, pad=-1):
+    def __init__(self, output_size, hidden_size, rnn_layers=(0, 2), embedding_dropout_p=0.1,
+                 sos=0, eos=1, pad=-1, max_length=100):
         super().__init__()
         self.output_size = output_size
         self.hidden_size = hidden_size
         self.sos = sos
         self.eos = eos
         self.pad = pad
+        self.max_length = max_length
 
         embedding = nn.Embedding(self.output_size, self.hidden_size)
         if embedding_dropout_p == 0:
@@ -129,39 +126,36 @@ class AttnDecoderRnn(nn.Module):
             rnn_layers[1] > 0 else None
         self.fc = nn.Linear(self.hidden_size, self.output_size)
 
-    def forward(self, encoder_outputs, encoder_output_lengths, input_labels=None, input_labels_lenghts=None,
-                max_length=100):
+    def forward(self, encoder_outputs, encoder_output_lengths, input_labels=None, input_labels_lenghts=None):
         teacher_forcing = input_labels is not None
+        device = encoder_outputs.get_device()
         batch_size = encoder_outputs.shape[0]
-        input_labels_start = torch.full([batch_size, 1], fill_value=self.sos, dtype=torch.long)
-        # input_labels_end = torch.full([batch_size, 1], fill_value=self.eos, dtype=torch.long)
+        input_labels_start = torch.full([batch_size, 1], fill_value=self.sos, dtype=torch.long, device=device)
         hidden1 = None
         hidden2 = None
         if teacher_forcing:
             input_labels = torch.cat((input_labels_start, input_labels), dim=1)
-            embedded = self.embedding(input_labels)
-            output = self.dropout(embedded)
+            output = self.embedding(input_labels)
             if self.rnn1 is not None:
                 # TODO: pack
                 output, hidden1 = self.rnn1(output, hidden1)
-            output, _ = self.attn(output, encoder_outputs)
+            output, _ = self.attn(output, encoder_outputs, encoder_output_lengths)
             if self.rnn2 is not None:
                 output, hidden2 = self.rnn2(output, hidden2)
             output = F.log_softmax(self.fc(output), dim=-1)
         else:
             input_labels = input_labels_start
             results = []
-            for _ in range(max_length):
-                embedded = self.embedding(input_labels)
-                output = self.dropout(embedded)
+            for _ in range(self.max_length):
+                output = self.embedding(input_labels)
                 if self.rnn1 is not None:
                     # TODO: pack
                     output, hidden1 = self.rnn1(output, hidden1)
-                output, _ = self.attn(output, encoder_outputs)
+                output, _ = self.attn(output, encoder_outputs, encoder_output_lengths)
                 if self.rnn2 is not None:
                     output, hidden2 = self.rnn2(output, hidden2)
                 output = F.log_softmax(self.fc(output), dim=-1)
                 results.append(output)
-                input_labels = torch.topk(output, 1, dim=-1)[1].unsqueeze(1)
-            output = torch.stack(results, dim=1)
+                input_labels = torch.topk(output, 1, dim=-1)[1].squeeze(-1)
+            output = torch.cat(results, dim=1)
         return output
