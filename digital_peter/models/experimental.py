8cc74d14c012238b8157a974f199ceb70a50032b
artbataev <artbataev@gmail.com>
2020-11-15 21:27:23 +0300

    Add experimental models

--- digital_peter/models/experimental.py
+++ digital_peter/models/experimental.py
@@ -11,3 +11,13 @@ def base__drop02__gru_2x256(num_outputs) -> nn.Module:
 def base__drop02__gru_4x128(num_outputs) -> nn.Module:
     model = BaselineModelBnAllNoTimePad(num_outputs=num_outputs, dropout=0.2, n_rnn=4, rnn_type="GRU", rnn_dim=128)
     return model
+
+
+def base__drop02__lstm_2x256(num_outputs) -> nn.Module:
+    model = BaselineModelBnAllNoTimePad(num_outputs=num_outputs, dropout=0.2, n_rnn=2, rnn_type="LSTM", rnn_dim=256)
+    return model
+
+
+def base__drop02__lstm_4x128(num_outputs) -> nn.Module:
+    model = BaselineModelBnAllNoTimePad(num_outputs=num_outputs, dropout=0.2, n_rnn=4, rnn_type="LSTM", rnn_dim=128)
+    return model
