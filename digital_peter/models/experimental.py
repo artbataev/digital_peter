cae4632a3e116c9abb06998152a3a1b34ebf3580
artbataev <artbataev@gmail.com>
2020-11-16 01:34:56 +0300

    Add more experimental modules

--- digital_peter/models/experimental.py
+++ digital_peter/models/experimental.py
@@ -39,6 +39,16 @@ def conv__gru_2x256_drop02(num_outputs) -> nn.Module:
     return model
 
 
+def conv__gru_2x368_drop02(num_outputs) -> nn.Module:
+    model = SequentialSequential(*[
+        ConvExtractor(),
+        LambdaModule(lambda seq, seq_len: (F.relu(seq), seq_len)),
+        RNNEncoder(dropout=0.2, rnn_type="GRU", num_layers=2, hidden_size=368, input_size=512),
+        SequentialLinear(256 * 2, num_outputs, pre_activation=True)
+    ])
+    return model
+
+
 def resnet__gru_2x256_drop02(num_outputs) -> nn.Module:
     model = SequentialSequential(*[
         ResnetExtractor(),
