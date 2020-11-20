d5a83d409d27f74077cfb84827666af43b530587
artbataev <artbataev@gmail.com>
2020-11-20 19:11:22 +0300

    Improve evaluation script

--- evaluate.py
+++ evaluate.py
@@ -7,7 +7,6 @@ import torch
 import torch.nn as nn
 from ctcdecode import CTCBeamDecoder
 from torch.utils.data import DataLoader
-import kaldi_io
 
 from digital_peter import models
 from digital_peter.data import DigitalPeterDataset, collate_fn
@@ -20,13 +19,16 @@ DATA_DIR = Path(__file__).parent / "data"
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--bs", type=int, default=10, help="batch size")
     parser.add_argument("--model", default="base", type=str)
     parser.add_argument("--from-ckp", type=str)
+    parser.add_argument("--img-height", type=int, default=128)
+    parser.add_argument("--bs", type=int, default=10, help="batch size")
+    parser.add_argument("--lmwt", type=float, default=1.0)
+    parser.add_argument("--wip", type=float, default=2.0)
     args = parser.parse_args()
 
     setup_logger()
-    log = logging.getLogger("trainscript")
+    log = logging.getLogger("evalscript")
     log.info(f"args: {args}")
 
     chars = get_chars_from_file(DATA_DIR / "chars_new.txt")
@@ -34,7 +36,9 @@ def main():
 
     with open(DATA_DIR / "val_uttids_set.pkl", "rb") as f:
         val_uttids = pickle.load(f)
-    val_data = DigitalPeterDataset(DATA_DIR / "train", val_uttids, encoder, image_len_divisible_by=4,
+    val_data = DigitalPeterDataset(DATA_DIR / "train", val_uttids,
+                                   encoder,
+                                   img_height=args.img_height, image_len_divisible_by=4,
                                    verbose=False, training=False)
     log.info(f"data: {len(val_data)}")
     num_outputs = len(encoder.id2char)
@@ -48,9 +52,9 @@ def main():
     phones_list[phones_list.index("]")] = "Q"
     parl_decoder = CTCBeamDecoder(
         phones_list,
-        model_path=f"{DATA_DIR / 'mixed666.gz'}",
-        alpha=1.0,
-        beta=2.0,
+        model_path=f"{DATA_DIR / 'lang_mixed_0.7/mixed'}",
+        alpha=args.lmwt,
+        beta=args.wip,
         cutoff_top_n=40,
         cutoff_prob=1.0,
         beam_width=100,
@@ -67,12 +71,12 @@ def main():
     learner = OcrLearner(model, None, criterion, None, val_loader, encoder, parl_decoder=parl_decoder)
 
     # learner.val_model()
-    # learner.val_model(greedy=False)
-    utt2logits = learner.get_val_logits()
-    with open("logits.ark", "wb") as f:
-        for key, torch_logits in utt2logits.items():
-            np_logits = torch_logits.numpy()
-            kaldi_io.write_mat(f, np_logits, key=key)
+    learner.val_model(greedy=False)
+    # utt2logits = learner.get_val_logits()
+    # with open("logits.ark", "wb") as f:
+    #     for key, torch_logits in utt2logits.items():
+    #         np_logits = torch_logits.numpy()
+    #         kaldi_io.write_mat(f, np_logits, key=key)
 
 
 if __name__ == "__main__":
