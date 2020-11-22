119cd6fcb888a544b3c8f5eb9f91914027b5884f
artbataev <artbataev@gmail.com>
2020-11-22 21:29:59 +0300

    Models clean up

--- digital_peter/models/experimental.py
+++ digital_peter/models/experimental.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from digital_peter.models.blocks import LambdaModule, SequentialLinear, SequentialSequential
-from digital_peter.models.conv import ConvExtractor, ResnetExtractor, ConvExtractorDeeper, ConvExtractorHiRes
+from digital_peter.models.conv import ConvExtractor, ResnetExtractor, ConvExtractorDeeper
 from digital_peter.models.rnn import RNNEncoder
 
 
@@ -28,40 +28,10 @@ def conv__gru_2x368_drop02(num_outputs) -> nn.Module:
     return model
 
 
-def resnet__gru_2x256_drop02(num_outputs) -> nn.Module:
-    model = SequentialSequential(*[
-        ResnetExtractor(),
-        LambdaModule(lambda seq, seq_len: (F.relu(seq), seq_len)),
-        RNNEncoder(dropout=0.2, rnn_type="GRU", num_layers=2, hidden_size=256, input_size=512),
-        SequentialLinear(256 * 2, num_outputs, pre_activation=True)
-    ])
-    return model
-
-
-def resnet__gru_2x368_drop02(num_outputs) -> nn.Module:
-    model = SequentialSequential(*[
-        ResnetExtractor(),
-        LambdaModule(lambda seq, seq_len: (F.relu(seq), seq_len)),
-        RNNEncoder(dropout=0.2, rnn_type="GRU", num_layers=2, hidden_size=368, input_size=512),
-        SequentialLinear(368 * 2, num_outputs, pre_activation=True)
-    ])
-    return model
-
-
 def resnet_pure(num_outputs) -> nn.Module:
     model = SequentialSequential(*[
         ResnetExtractor(),
         SequentialLinear(512, num_outputs, pre_activation=True)
-    ])
-    return model
-
-
-def conv_deeper__gru_2x256_drop02(num_outputs) -> nn.Module:
-    model = SequentialSequential(*[
-        ConvExtractorDeeper(),
-        LambdaModule(lambda seq, seq_len: (F.relu(seq), seq_len)),
-        RNNEncoder(dropout=0.2, rnn_type="GRU", num_layers=2, hidden_size=256, input_size=512),
-        SequentialLinear(256 * 2, num_outputs, pre_activation=True)
     ])
     return model
 
@@ -74,32 +44,3 @@ def conv_deeper__gru_2x368_drop02(num_outputs) -> nn.Module:
         SequentialLinear(368 * 2, num_outputs, pre_activation=True)
     ])
     return model
-
-
-def conv_hires__gru_2x256_drop02(num_outputs) -> nn.Module:
-    model = SequentialSequential(*[
-        ConvExtractorHiRes(),
-        LambdaModule(lambda seq, seq_len: (F.relu(seq), seq_len)),
-        RNNEncoder(dropout=0.2, rnn_type="GRU", num_layers=2, hidden_size=256, input_size=512),
-        SequentialLinear(256 * 2, num_outputs, pre_activation=True)
-    ])
-    return model
-
-
-def conv_hires__gru_2x368_drop02(num_outputs) -> nn.Module:
-    model = SequentialSequential(*[
-        ConvExtractorHiRes(),
-        LambdaModule(lambda seq, seq_len: (F.relu(seq), seq_len)),
-        RNNEncoder(dropout=0.2, rnn_type="GRU", num_layers=2, hidden_size=368, input_size=512),
-        SequentialLinear(368 * 2, num_outputs, pre_activation=True)
-    ])
-    return model
-
-# def convhires__gru_2x256_drop02(num_outputs) -> nn.Module:
-#     model = SequentialSequential(*[
-#         ConvExtractor(),
-#         LambdaModule(lambda seq, seq_len: (F.relu(seq), seq_len)),
-#         RNNEncoder(dropout=0.2, rnn_type="GRU", num_layers=2, hidden_size=256, input_size=512),
-#         SequentialLinear(256 * 2, num_outputs, pre_activation=True)
-#     ])
-#     return model
--- digital_peter/models/experimental_old.py
+++ digital_peter/models/experimental_old.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 from digital_peter.models.base import BaselineModelBnAllNoTimePad
 from digital_peter.models.blocks import LambdaModule, SequentialLinear, SequentialSequential
-from digital_peter.models.conv import ConvExtractor, ResnetExtractor
+from digital_peter.models.conv import ConvExtractor, ResnetExtractor, ConvExtractorHiRes, ConvExtractorDeeper
 from digital_peter.models.rnn import RNNEncoder, TransformerEncoder
 
 
@@ -94,5 +94,55 @@ def conv__gru_3x368_drop02(num_outputs) -> nn.Module:
         LambdaModule(lambda seq, seq_len: (F.relu(seq), seq_len)),
         RNNEncoder(dropout=0.2, rnn_type="GRU", num_layers=3, hidden_size=368, input_size=512),
         SequentialLinear(368 * 2, num_outputs, pre_activation=True)
+    ])
+    return model
+
+
+def conv_hires__gru_2x256_drop02(num_outputs) -> nn.Module:
+    model = SequentialSequential(*[
+        ConvExtractorHiRes(),
+        LambdaModule(lambda seq, seq_len: (F.relu(seq), seq_len)),
+        RNNEncoder(dropout=0.2, rnn_type="GRU", num_layers=2, hidden_size=256, input_size=512),
+        SequentialLinear(256 * 2, num_outputs, pre_activation=True)
+    ])
+    return model
+
+
+def conv_hires__gru_2x368_drop02(num_outputs) -> nn.Module:
+    model = SequentialSequential(*[
+        ConvExtractorHiRes(),
+        LambdaModule(lambda seq, seq_len: (F.relu(seq), seq_len)),
+        RNNEncoder(dropout=0.2, rnn_type="GRU", num_layers=2, hidden_size=368, input_size=512),
+        SequentialLinear(368 * 2, num_outputs, pre_activation=True)
+    ])
+    return model
+
+
+def resnet__gru_2x256_drop02(num_outputs) -> nn.Module:
+    model = SequentialSequential(*[
+        ResnetExtractor(),
+        LambdaModule(lambda seq, seq_len: (F.relu(seq), seq_len)),
+        RNNEncoder(dropout=0.2, rnn_type="GRU", num_layers=2, hidden_size=256, input_size=512),
+        SequentialLinear(256 * 2, num_outputs, pre_activation=True)
+    ])
+    return model
+
+
+def resnet__gru_2x368_drop02(num_outputs) -> nn.Module:
+    model = SequentialSequential(*[
+        ResnetExtractor(),
+        LambdaModule(lambda seq, seq_len: (F.relu(seq), seq_len)),
+        RNNEncoder(dropout=0.2, rnn_type="GRU", num_layers=2, hidden_size=368, input_size=512),
+        SequentialLinear(368 * 2, num_outputs, pre_activation=True)
+    ])
+    return model
+
+
+def conv_deeper__gru_2x256_drop02(num_outputs) -> nn.Module:
+    model = SequentialSequential(*[
+        ConvExtractorDeeper(),
+        LambdaModule(lambda seq, seq_len: (F.relu(seq), seq_len)),
+        RNNEncoder(dropout=0.2, rnn_type="GRU", num_layers=2, hidden_size=256, input_size=512),
+        SequentialLinear(256 * 2, num_outputs, pre_activation=True)
     ])
     return model
